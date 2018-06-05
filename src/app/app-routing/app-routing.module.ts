3d24fe974e749942554de575c25dc7d46845ab49
corrado1992 <37770025+corrado1992@users.noreply.github.com>
2018-06-05 17:53:53 +0200

    Aggiunto guard a "/confirmation"

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,12 +3,20 @@ import {RouterModule, Routes} from '@angular/router';
 import {MapComponent} from '../components/map/map.component';
 import {CustomerbuyComponent} from '../components/map/customerbuy/customerbuy.component';
 import {CustomerPurchaseComponent} from '../components/map/customer-purchase/customer-purchase.component';
+import {AuthGuardGuard} from '../auth-guard.guard';
+import {ShareMapInfoService} from '../services/share-map-info.service';
 
 const routes: Routes = [
-  {path: 'positions', component: MapComponent,
-  children: [
-    {path: '', component: CustomerbuyComponent},
-    {path: 'confirmation', component: CustomerPurchaseComponent}
+  {
+      path: 'positions', component: MapComponent, children: [
+    {
+        path: '', component: CustomerbuyComponent
+    },
+    {
+        path: 'confirmation',
+        canActivate: [AuthGuardGuard],
+        component: CustomerPurchaseComponent
+    }
   ]},
   {path: '', redirectTo: 'positions', pathMatch: 'full'}
 ];
@@ -18,6 +26,7 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes, {enableTracing: true})
   ],
+    providers: [AuthGuardGuard, ShareMapInfoService],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
---
+++ src/app/auth-guard.guard.ts
@@ -0,0 +1,18 @@
+ import { Injectable } from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
+ import {ShareMapInfoService} from './services/share-map-info.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardGuard implements CanActivate {
+    constructor(private customer: ShareMapInfoService, private router: Router) {}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+      if ( this.customer.customerRequest == null || this.customer.customerRequest.polygon == null ) {
+          this.router.navigate(['/positions']);
+      }
+      return true;
+  }
+}
