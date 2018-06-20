abac9150392445ceeda5dd400829eff9b706e149
Nico Caprioli <nico.caprioli@gmail.com>
2018-06-21 00:42:22 +0200

    Vista invio file utente. Per motivi ignoti il mime-type dei json viene settato a text/plain e quindi il server lo rigetta.

--- src/app/components/logged/user-upload/user-upload.component.ts
+++ src/app/components/logged/user-upload/user-upload.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Urls, UrlService} from "../../../services/url.service";
-import {FileUploader} from 'ng2-file-upload';
+import {FileUploader, FileUploaderOptions, FilterFunction} from 'ng2-file-upload';
 import {UserTokenService} from "../../../services/user.service";
+import {FileLikeObject} from "ng2-file-upload/file-upload/file-like-object.class";
 
 @Component({
   selector: 'app-user-upload',
@@ -15,11 +16,21 @@ export class UserUploadComponent implements OnInit {
   displayedColumns = ['name', 'size', 'action', 'progress'];
 
   constructor(baseService: UrlService, user: UserTokenService) {
+    const filter: FilterFunction = {
+      name: 'json',
+      fn: (item: FileLikeObject) => {
+        const name = item.name;
+        return (name.lastIndexOf('json') === name.length - 4);
+      }
+    };
     baseService.promise.then((urlList: Urls) =>
-        this.uploader = new FileUploader({
+        this.uploader.setOptions({
           url: urlList.userPositions.href,
-          disableMultipart: true
-        }));
+          disableMultipart: true,
+          authToken: `Bearer ${user.accessToken}`,
+          filters: [filter]
+        })
+    );
   }
 
   public fileOverBase(e: any): void {
