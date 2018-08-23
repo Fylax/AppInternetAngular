6784589b2289a7a7a7d6c40cd6508f5e786e2842
corrado1992 <37770025+corrado1992@users.noreply.github.com>
2018-08-24 00:17:55 +0200

    mapComponent: inserito @Input 'drawable' e gestite le drawOptions. Da verificare quando sputare fuori il polygon in output.

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output, OnInit} from '@angular/core';
 import * as L from 'leaflet';
 import {ApproximatedArchive} from '../model/approximated-archive';
 import {Point} from '../model/point';
@@ -8,7 +8,7 @@ import {Point} from '../model/point';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent {
+export class MapComponent implements OnInit {
 
   options = {
     zoomControl: true,
@@ -21,16 +21,17 @@ export class MapComponent {
     zoom: 13,
     center: L.latLng(45.06495, 7.66155)
   };
+
   private editableLayers = new L.FeatureGroup();
-  private hidden = false;
   private map: L.Map;
 
+  private currPolygon;
   archives: ApproximatedArchive[];
   private markerLayers: L.LayerGroup;
 
-  @Input() set setItems(value: ApproximatedArchive[]) {
-    this.markerLayers.remove();
-    this.markerLayers.clearLayers();
+    @Input() drawable: boolean;
+    @Input() set setItems(value: ApproximatedArchive[]) {
+    this.markerLayers = new L.LayerGroup();
     this.archives = value;
     if (this.archives !== undefined) {
       for (let i = 0; i < this.archives.length; i++) {
@@ -59,7 +60,59 @@ export class MapComponent {
   }
 
   constructor() {
+      this.drawable = true;
   }
+
+    ngOnInit() {
+        this.map.addLayer(this.editableLayers);
+
+        if (this.drawable) {
+            const drawControl = new L.Control.Draw({
+                edit: {
+                    featureGroup: this.editableLayers
+                },
+                position: 'topright',
+                draw: {
+                    marker: false,
+                    circlemarker: false,
+                    rectangle: false,
+                    polyline: false,
+                    circle: false,
+                    polygon: {
+                        allowIntersection: false, // Restricts shapes to simple polygons
+                        drawError: {
+                            color: 'red', // Color the shape will turn when intersects
+                            message: '<strong>Non consentito!<strong>'
+                        },
+                    },
+                }
+            });
+          this.map.addControl(drawControl);
+        }
+        this.map
+            .on(L.Draw.Event.CREATED, (e: L.DrawEvents.Created) => {
+                if (e.type !== 'draw:created' && e.layerType !== 'polygon') {
+                    return;
+                }
+                this.currPolygon = (e.layer as L.Polygon);
+                const controls = document.getElementsByClassName('leaflet-draw-toolbar');
+                (controls[0] as HTMLDivElement).style.display = 'none';
+                (controls[1] as HTMLDivElement).style.display = 'block';
+            })
+            .on(L.Draw.Event.DELETED, (e: L.DrawEvents.Deleted) => {
+                if (e.layers.getLayers().length !== 0) {
+                    const controls = document.getElementsByClassName('leaflet-draw-toolbar');
+                    (controls[0] as HTMLDivElement).style.display = 'block';
+                    (controls[1] as HTMLDivElement).style.display = 'none';
+                }
+            })
+            .on(L.Draw.Event.EDITED, (e: L.DrawEvents.Edited) => {
+                const poly = (e.layers.getLayers()[0] as L.Polygon);
+                if (poly !== undefined) {
+                    this.currPolygon = poly;
+                }
+            });
+    }
 
   private createPolygonFromBounds(latLngBounds) {
     const center = latLngBounds.getCenter();
@@ -83,7 +136,8 @@ export class MapComponent {
   }
 
   onMapChange() {
-    const polygon = this.createPolygonFromBounds(this.map.getBounds());
-    this.polygonOutput.emit(polygon);
+    this.currPolygon = this.createPolygonFromBounds(this.map.getBounds());
+    this.polygonOutput.emit(this.currPolygon);
+    /*** da verificare quando emettere il polygon, così torna sempre la mappa (rettangolo) ??? ***/
   }
 }
